fc6ce852f4099554dde32bcf6919c5131e802834
mmeyrat <maximemeyrat@orange.fr>
2021-11-03 15:08:56 +0100

    number command added

---
+++ commands/number.js
@@ -0,0 +1,34 @@
+module.exports = {
+	name: "nombre",
+	title: "Activer/désactiver les nombres",
+	arguments: [""],
+	description: "Active ou désactive les réponses aux nombres. Quand un nombre est envoyé, le bot envoie le nombre suivant.",
+	example: "",
+	execute(chan, guild, args) {
+		const Discord = require("discord.js");
+		const fs = require("fs");
+
+		let data = JSON.parse(fs.readFileSync("data.json", 'utf8'));
+
+		if (data.servers[guild] == null) {
+			data.servers[guild] = {};
+		}
+
+		let msg;
+		if (data.servers[guild].isNumberEnabled == null) {
+			data.servers[guild].isNumberEnabled = true;
+			msg = "Les réponses aux nombres sont activées";
+		} else {
+			delete data.servers[guild].isNumberEnabled;
+			msg = "Les réponses aux nombres sont désactivées";
+		}
+
+		let json = JSON.stringify(data, null, "\t");
+		fs.writeFileSync("data.json", json);
+
+		let embed = new Discord.MessageEmbed()
+			.setColor("#AC8A4D")
+			.setDescription(msg);
+		chan.send(embed);
+	}
+}
